7c35a52d83293427e5eb6eebb5258c307e6cd0f3
lseshan <lsimhan.cse@gmail.com>
2017-03-06 15:39:11 -0800

    LN changes for naive_bayes author

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -14,12 +14,28 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
-
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
+
+## Create a Gaussian Classifier
+clf = GaussianNB()
+## Train the system
+## Features are properties labels are the identifiers
+
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+t1=time()
+pred = clf.predict(features_test)
+print "training time:", round(time()-t1, 3), "s"
+
+print accuracy_score(pred, labels_test)
 
 
 
